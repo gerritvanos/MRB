df34413a4d8e1c6fe26ff2386d5cd9bd1f48b087
gerritvanos <gerrit.vanos@student.hu.nl>
2019-05-27 15:44:41 +0200

    Update test_follow_ball.py

--- test_follow_ball.py
+++ test_follow_ball.py
@@ -1,26 +1,27 @@
-from collections import deque
-import numpy as np
 import datetime
 import cv2
 import serial
 
 def send_x(value,arduino):
     data =  "X " + str(int(value)) + "\n"
+    print(data)
     data = data.encode("ascii")
+
     arduino.write(data)
 
 
 def send_y(value,arduino):
-    data = "Y " + str(int(value)) + "\n"
+    data = "Y " + str(int(value)/2) + "\n"
+    print(data)
     data = data.encode("ascii")
     arduino.write(data)
 
 
 class PID:
     def __init__(self,dt,res):
-        self.kp = 0
-        self.ki = 0
-        self.kd = 0
+        self.kp = [9,9]
+        self.ki = [0.25,0.25]
+        self.kd = [4,4]
         self.set_point = [260,150]
         self.total_errors = [0,0]
         self.prev_error = [0,0]
@@ -29,24 +30,24 @@ class PID:
 
     def config_set_point(self,new_set_point):
         self.set_point = new_set_point
+        self.total_errors = [0,0]
 
     def calculate_single_PID(self,current_values, axis):
         current_error = self.set_point[axis] - current_values[axis]
-        self.total_errors[axis] += current_error
 
+        self.total_errors[axis] += current_error
         d_action = (current_error - self.prev_error[axis])/self.dt
 
         i_action = self.total_errors[axis] * self.dt
-        total_action = (current_error * self.kp) + (d_action * self.kd) + (i_action * self.ki)
+        total_action = (current_error * self.kp[axis]) + (d_action * self.kd[axis]) + (i_action * self.ki[axis])
 
         self.prev_error[axis] = current_error
 
-        return (total_action /1000)
+        return (total_action /75)
 
 
     def caluclate_PID(self,current_values):
         return [self.calculate_single_PID(current_values, 0), self.calculate_single_PID(current_values, 1)] #calc x,y
-        #return self.calculate_single_PID(current_values, 1)
 
 arduino = serial.Serial("COM2",115200)
 
@@ -54,7 +55,7 @@ ball_lower = (0, 46, 128)
 ball_upper = (30, 121, 255)
 
 camera = cv2.VideoCapture(1)
-camera.set(cv2.CAP_PROP_FPS, 60)
+camera.set(cv2.CAP_PROP_FPS, 120)
 fps = camera.get(cv2.CAP_PROP_FPS)
 
 dt = 1/fps
@@ -74,7 +75,6 @@ def mouse_drawing(event, x, y, flags, params):
     global set_point
     if event == cv2.EVENT_LBUTTONDOWN:
             set_point = (x, y)
-
 
 
 while True:
@@ -97,18 +97,22 @@ while True:
         c = max(cnts, key=cv2.contourArea)
         ((current_x, current_y), radius) = cv2.minEnclosingCircle(c)
 
-        current_values = [current_x,current_y]
-        pid_value = servo_pid.caluclate_PID(current_values)
-        #send_x(pid_value[0],arduino)
-        send_y(pid_value[1],arduino)
         if radius > 10 and radius < 20:
             cv2.circle(frame, (int(current_x), int(current_y)), int(radius), (0, 255, 255), 2)
+            current_values = [current_x, current_y]
+            pid_value = servo_pid.caluclate_PID(current_values)
+            send_x(pid_value[0], arduino)
+            send_y(pid_value[1], arduino)
+
+        cv2.circle(frame, (set_point[0], set_point[1]), int(5), (0, 255, 127), 2)
 
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
     end = datetime.datetime.now()
+
     while((end - start).seconds >= dt):
         end = datetime.datetime.now()
+
     key = cv2.waitKey(1) & 0xFF
 
     if key == ord("q"):
@@ -127,10 +131,6 @@ while True:
         servo_pid.ki -= 0.1
     elif key == ord("m"):
         servo_pid.ki += 0.1
-
-    print("kp", servo_pid.kp)
-    print("kd", servo_pid.kd)
-    print("ki", servo_pid.ki)
 
 camera.release()
 cv2.destroyAllWindows()
